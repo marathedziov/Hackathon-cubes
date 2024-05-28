d43fc465a4c7575918f436e007c2eeb919188478
odziov <mrtdzv@gmail.com>
2024-05-28 18:32:47 +0300

    пока так сделал

--- app.py
+++ app.py
@@ -5,7 +5,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    flags = {
+        'module1_flag': False,
+        'task1_1_flag': False,
+        'task2_1_flag': False,
+        'task3_1_flag': False,
+        'task4_1_flag': False,
+        'module2_flag': False,
+        'task1_2_flag': False,
+        'task2_2_flag': False,
+        'task3_2_flag': False,
+        'task4_2_flag': False,
+        'module3_flag': False,
+        'task1_3_flag': False,
+        'task2_3_flag': False,
+        'task3_3_flag': False,
+        'task4_3_flag': False
+    }
+    return render_template('index.html', **flags)
 
 
 if __name__ == '__main__':
