cdce56a7daa09a4812ec6ebadc7a3722831ce8ca
odziov <mrtdzv@gmail.com>
2024-06-02 20:39:18 +0300

    работает??

--- app.py
+++ app.py
@@ -3,16 +3,10 @@ import json
 from flask import Flask, render_template
 
 
-def data_json():
+def get_data_json():
     with open('level1.json', 'r') as f:
         data = json.load(f)
-        lst_for_btn = []
-        for module in data['modules']:
-            lst_for_btn.append((module['sizes'], module['coordinates']))
-            for task in module['tasks']:
-                lst_for_btn.append((task['sizes'], task['coordinates']))
-        print(lst_for_btn)
-        return lst_for_btn
+        return data
 
 
 app = Flask(__name__)
@@ -37,8 +31,10 @@ def index():
         'task3_3_flag': False,
         'task4_3_flag': False
     }
-    lst_for_btn = data_json()
-    return render_template('index.html', lst_for_btn=lst_for_btn, **flags)
+    data_json = get_data_json()
+    print(data_json)
+    # print(data_json[])
+    return render_template('index.html', data=data_json, **flags)
 
 
 if __name__ == '__main__':
