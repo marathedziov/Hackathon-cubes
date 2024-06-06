154b2efaa6acc66bffb2d0b83347ca8fe0cfb643
DEIRIl <153659139+DEIRIl@users.noreply.github.com>
2024-06-06 19:16:45 +0300

    added loading equations into the database

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, abort, request
 from data import db_session
-from data.level_module_task import Module
+from data.level_module_task import Module, Progress
 from data.users import User
 from forms.user import RegisterForm, LoginForm
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
@@ -78,8 +78,7 @@ def level_flowers():
     return render_template('level_flowers.html', equations=equations)
 
 
-@app.route('/solv/<int:lvl><int:module><int:ind>', methods=['GET', 'POST'])
-def solving_eq(lvl, module, ind):
+def solving_eq(eq):
     if request.method == 'POST':
         ans = request.form.get('ans')
         result = request.form.get('result')
@@ -87,16 +86,26 @@ def solving_eq(lvl, module, ind):
             return render_template('level_flower_res.html', res="ПРАВИЛЬНО")
         return render_template('level_flower_res.html', res="НЕПРАВИЛЬНО")
     else:
-        # db_sess = db_session.create_session()
-        # eq = db_sess.query(Module).filter(Module.level_id == lvl)[module][ind]
-        eq = gen_equations.gen_eq(12)
         return render_template('solving_eq.html', eq=eq)
 
-#
-# @app.route('/flowers', methods=['GET', 'POST'])
-# def gen_eq():
 
-
+@app.route('/solv/<int:lvl><int:module><int:task>', methods=['GET', 'POST'])
+def genering(lvl, module, task):
+    equat = gen_equations.gen_eq(int(f'{lvl}{module}'))
+    db_sess = db_session.create_session()
+    eqs = db_sess.query(Progress).filter(Progress.id == (lvl - 1) * 12 * (module - 1) * 4 + (task)).first()
+    if eqs:
+        solving_eq((eqs.text_task, eqs.answer))
+    else:
+        progress = Progress()
+        progress.user_id = current_user
+        progress.level_id = lvl
+        progress.module_id = module
+        progress.text_task = equat[0]
+        progress.answer = equat[1]
+        db_sess.add(progress)
+        db_sess.commit()
+        solving_eq(equat)
 
 
 if __name__ == "__main__":
